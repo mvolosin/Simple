9c690e8dff47a88a706ef19f6cc5c0a027f2ef63
Jan Valiska <jan.valiska@gmail.com>
2017-05-23 22:13:43 +0200

    pool added

---
+++ tests/thread_pool_test.cpp
@@ -0,0 +1,31 @@
+#include "../include/ThreadPool.hpp"
+#include "../include/Logger.hpp"
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void task()
+{
+    size_t s = 0;
+    for (; s < 100000; ++s){
+        ;
+    }
+    LOG(Simple::LogLevel::INFO) << "Result = " << s;
+};
+
+int main(int argc, char *argv[])
+{
+    Simple::ThreadPool<void()> pool(10);
+
+    std::vector<std::future<void>> futures;
+    for (size_t i = 0; i < 100; ++i) {
+        futures.push_back(pool.addTask([]() { task(); }));
+    }
+
+    for(auto& f : futures){
+        f.get();
+    }
+
+    return 0;
+}
