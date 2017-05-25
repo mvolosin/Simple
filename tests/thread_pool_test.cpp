4c3895e39c5ef40e969d35991de5850ac5699be2
Jan Valiska <jan.valiska@gmail.com>
2017-05-25 21:17:32 +0200

    ThreadPool works also for not void functions. Yupiii.

--- tests/thread_pool_test.cpp
+++ tests/thread_pool_test.cpp
@@ -6,13 +6,23 @@
 
 using namespace std;
 
-void task()
+void task(size_t id)
 {
     size_t s = 0;
     for (; s < 100000; ++s){
         ;
     }
-    LOG(LogLevel::INFO) << "Result = " << s;
+    LOG(LogLevel::INFO) << "Task ID " << id << " result = " << s;
+};
+
+size_t task2(size_t id)
+{
+    size_t s = 0;
+    for (; s < 100000; ++s){
+        ;
+    }
+    LOG(LogLevel::INFO) << "Task ID " << id << " result = " << s;
+    return s;
 };
 
 int main(int argc, char *argv[])
@@ -21,11 +31,22 @@ int main(int argc, char *argv[])
 
     std::vector<std::future<void>> futures;
     for (size_t i = 0; i < 100; ++i) {
-        futures.push_back(pool.addTask([]() { task(); }));
+        futures.push_back(pool.addTask([=]() { task(i); }));
     }
 
     for(auto& f : futures){
         f.get();
+    }
+
+    Simple::ThreadPool<size_t()> pool2(10);
+
+    std::vector<std::future<size_t>> futures2;
+    for (size_t i = 0; i < 100; ++i) {
+        futures2.push_back(pool2.addTask([i]() { return task2(i); }));
+    }
+
+    for(auto& f : futures2){
+        auto s = f.get();
     }
 
     return 0;
