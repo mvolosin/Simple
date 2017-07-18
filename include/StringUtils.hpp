7076c369f6eb41b08d2c78ddbdbcb71fe3346185
Janko Valiska <janko@valiska.sk>
2017-07-18 19:24:57 +0200

    Added comments to string utils

--- include/StringUtils.hpp
+++ include/StringUtils.hpp
@@ -17,22 +17,46 @@ template <typename CharT>
 using tstringstream = std::basic_stringstream<CharT, std::char_traits<CharT>,
                                               std::allocator<CharT>>;
 
+/**
+ * @brief Converts std::basic_string to upper case
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string with all upper case characters
+ */
 template <typename CharT>
 inline tstring<CharT> to_upper(tstring<CharT> text)
 {
-    std::transform(std::begin(text), std::end(text),
-                   std::begin(text), std::toupper);
+    std::transform(std::begin(text), std::end(text), std::begin(text),
+                   std::toupper);
     return text;
 }
 
+/**
+ * @brief Converts std::basic_string to lower case
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string with all lower case characters
+ */
 template <typename CharT>
 inline tstring<CharT> to_lower(tstring<CharT> text)
 {
-    std::transform(std::begin(text), std::end(text),
-                   std::begin(text), std::tolower);
+    std::transform(std::begin(text), std::end(text), std::begin(text),
+                   std::tolower);
     return text;
 }
 
+/**
+ * @brief Reverse std:basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string with all characters reversed
+ */
 template <typename CharT>
 inline tstring<CharT> reverse(tstring<CharT> text)
 {
@@ -40,53 +64,113 @@ inline tstring<CharT> reverse(tstring<CharT> text)
     return text;
 }
 
+/**
+ * @brief Trim all spaces from start and end of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string trimmed from beginning and end
+ */
 template <typename CharT>
-inline tstring<CharT> trim(tstring<CharT> const& text)
+inline tstring<CharT> trim(tstring<CharT> const &text)
 {
-    auto first {text.find_first_not_of(' ')};
-    auto last  {text.find_last_not_of(' ')};
+    auto first{text.find_first_not_of(' ')};
+    auto last{text.find_last_not_of(' ')};
     return text.substr(first, (last - first + 1));
 }
 
+/**
+ * @brief Trim all spaces from start of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string trimmed from beginning
+ */
 template <typename CharT>
-inline tstring<CharT> trimleft(tstring<CharT> const& text)
+inline tstring<CharT> trimleft(tstring<CharT> const &text)
 {
-    auto first {text.find_first_not_of(' ')};
+    auto first{text.find_first_not_of(' ')};
     return text.substr(first, text.size() - first);
 }
 
+/**
+ * @brief Trim all spaces from end of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ *
+ * @return std::basic_string trimmed from end
+ */
 template <typename CharT>
-inline tstring<CharT> trimright(tstring<CharT> const& text)
+inline tstring<CharT> trimright(tstring<CharT> const &text)
 {
-    auto last {text.find_last_not_of(' ')};
+    auto last{text.find_last_not_of(' ')};
     return text.substr(0, last + 1);
 }
 
+/**
+ * @brief Trim all defined characters from begining and end of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ * @param chars character that should be trimmed
+ *
+ * @return std::basic_string trimmed from beginning and end
+ */
 template <typename CharT>
-inline tstring<CharT> trim(tstring<CharT> const& text,
-                           tstring<CharT> const& chars)
+inline tstring<CharT> trim(tstring<CharT> const &text,
+                           tstring<CharT> const &chars)
 {
-    auto first {text.find_first_not_of(chars)};
-    auto last  {text.find_last_not_of(chars)};
+    auto first{text.find_first_not_of(chars)};
+    auto last{text.find_last_not_of(chars)};
     return text.substr(first, (last - first + 1));
 }
 
+/**
+ * @brief Trim all defined characters from begining of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ * @param chars character that should be trimmed
+ *
+ * @return std::basic_string trimmed from beginning
+ */
 template <typename CharT>
-inline tstring<CharT> trimleft(tstring<CharT> const& text,
-                               tstring<CharT> const& chars)
+inline tstring<CharT> trimleft(tstring<CharT> const &text,
+                               tstring<CharT> const &chars)
 {
-    auto first {text.find_first_not_of(chars)};
+    auto first{text.find_first_not_of(chars)};
     return text.substr(first, text.size() - first);
 }
 
+/**
+ * @brief Trim all defined characters from end of std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ * @param chars character that should be trimmed
+ *
+ * @return std::basic_string trimmed from end
+ */
 template <typename CharT>
-inline tstring<CharT> trimright(tstring<CharT> const& text,
-                                tstring<CharT> const& chars)
+inline tstring<CharT> trimright(tstring<CharT> const &text,
+                                tstring<CharT> const &chars)
 {
-    auto last {text.find_last_not_of(chars)};
+    auto last{text.find_last_not_of(chars)};
     return text.substr(0, last + 1);
 }
 
+/**
+ * @brief Remove all specified characters from std::basic_string
+ *
+ * @tparam CharT type of character
+ * @param text input text
+ * @param ch Character that should be removed from string
+ *
+ * @return  std::basic_string with removed characters
+ */
 template <typename CharT>
 inline tstring<CharT> remove(tstring<CharT> text, CharT const ch)
 {
@@ -96,20 +180,27 @@ inline tstring<CharT> remove(tstring<CharT> text, CharT const ch)
     return text;
 }
 
+/**
+ * @brief Split std::basic_string by specified delimiter
+ *
+ * @tparam CharT type of characters
+ * @param text input text
+ * @param delimiter Character by with input text should be splitted
+ *
+ * @return Vector of substrings gathered by splitting input text
+ */
 template <typename CharT>
-inline std::vector<tstring<CharT>> split(
-    tstring<CharT> text,
-    CharT const delimiter)
+inline std::vector<tstring<CharT>> split(tstring<CharT> text,
+                                         CharT const delimiter)
 {
     tstringstream<CharT> sstr{text};
     std::vector<tstring<CharT>> tokens;
     tstring<CharT> token;
-    while(std::getline(sstr, token, delimiter)){
-        if(!token.empty()) tokens.push_back(token);
+    while (std::getline(sstr, token, delimiter)) {
+        if (!token.empty()) tokens.push_back(token);
     }
     return tokens;
 }
-
 }
 }
 
